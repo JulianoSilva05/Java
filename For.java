e48594e1547afe7aadf86f42c3e3aa77a5cb98d8
Juliano Silva <57194781+JulianoSilva05@users.noreply.github.com>
2023-10-02 11:31:18 -0300

    loop

--- For.java
+++ For.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 public class For {
     public static void main(String[] args) {
-        //FOR
-        for(int cont=0 ; cont < 10 ; cont++){ //for(inicialização ; Controle ; Incremento )
+        //FOR - Já sei quantas vezes meu loop vai rodar
+        //Incremento
+        for(int cont=1 ; cont <= 10 ; cont++){ //for(inicialização ; Controle ; Incremento )
+            System.out.println(cont+" - Incremento");
 
         }
+        System.out.println("Fim do programa!");
+        System.out.println("\n**********************\n");
+        //decremento
+        for(int cont=10 ; cont >= 0 ; cont--){ //for(inicialização ; Controle ; Incremento )
+            System.out.println(cont+" - Decremento");
+
+        }
+        System.out.println("Fim do programa!");
     }
 }
---
+++ While.java
@@ -0,0 +1,19 @@
+public class While {
+   public static void main(String[] args) {
+      // While - usando quando náo sei quantas vezes o loop repete
+      int cont = 0;
+      while (cont <= 10) {
+         System.out.println(cont + " - incremento");
+         cont = cont+1;
+      }
+   }
+
+}
+
+   
+       
+      
+        
+         
+      
+   
