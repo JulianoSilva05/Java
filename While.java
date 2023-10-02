d9daca822e6a6aacb07baf1f4272cea1a41b8157
Juliano Silva <57194781+JulianoSilva05@users.noreply.github.com>
2023-10-02 11:41:42 -0300

    while

--- While.java
+++ While.java
@@ -1,19 +1,20 @@
+import java.util.Scanner;
+
 public class While {
    public static void main(String[] args) {
-      // While - usando quando náo sei quantas vezes o loop repete
+      Scanner ler = new Scanner(System.in);
+
+      System.out.print("Digite uma palavra: ");
+      String palavra = ler.nextLine();
+
+      System.out.print("Digite a quantidade de vezes que a palavra deve repetir: ");
+      int quant = ler.nextInt(); 
+      
+      // While - usando quando não sei quantas vezes o loop repete
       int cont = 0;
-      while (cont <= 10) {
-         System.out.println(cont + " - incremento");
-         cont = cont+1;
+      while (cont < quant) {
+         System.out.println(cont + " " + palavra);
+         cont = cont + 1;
       }
    }
-
 }
-
-   
-       
-      
-        
-         
-      
-   
